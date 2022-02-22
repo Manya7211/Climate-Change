ff8c2b0d11f7dee5410c7b0caa00a214d7b6941a
Manya7211 <83996642+Manya7211@users.noreply.github.com>
2022-02-22 08:30:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function secondScreen() {
+    window.location = "Second_Screen.html";
+}
+
+function thirdScreen() {
+    window.location = "Third_Screen.html";
+}
+
+function fourthScreen() {
+    window.location = "Fourth_Screen.html";
+}
